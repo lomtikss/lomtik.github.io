84f514609b9e0e7ce8c8773a5440e2d93feac8c2
lomtikss <rinatgolub86@gmail.com>
2026-08-10 11:40:44 +0300

    345

--- data.js
+++ data.js
@@ -97,4 +97,5 @@ const animeData = [
   { title: "Унесённые призраками", titleEn: "Spirited Away", cover: "images/wow.jpg", rating: 4 },
   { title: "Второй размер, интим предлагать", titleEn: "Yamada's First Time", cover: "images/dvarazmer.jpg", rating: 4 },
   { title: "Магическая битва", titleEn: "Jujutsu Kaisen", cover: "images/magica.jpg", rating: 3.5 },
+  { title: "Заботливая 800-летняя жена!", titleEn: "The Helpful Fox Senko-san", cover: "images/senko.jpg", rating: 4 },
 ];
