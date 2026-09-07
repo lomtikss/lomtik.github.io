893238c93aa79ae154a76173a5b436e792a5b050
lomtikss <rinatgolub86@gmail.com>
2026-09-07 18:52:12 +0300

    фів

--- data.js
+++ data.js
@@ -100,4 +100,5 @@ const animeData = [
   { title: "Заботливая 800-летняя жена!", titleEn: "The Helpful Fox Senko-san", cover: "images/senko.jpg", rating: 4 },
   { title: "Морская невеста", titleEn: "My Bride is a Mermaid", cover: "images/more.jpg", rating: 4.5 },
   { title: "Достучаться до тебя", titleEn: "Kimi ni Todoke: From Me to You", cover: "images/tebya.jpg", rating: 3.5 },
+  { title: "Фури-Кури", titleEn: "Fooly Cooly", cover: "images/fury.jpg", rating: 4.5 },
 ];
